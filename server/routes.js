44ac8b33e287bd90e22c19cdccc8505511afa1b2
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-06-14 16:10:06 -0700

    fix: require paths for endpoints

--- server/routes.js
+++ server/routes.js
@@ -3,15 +3,14 @@ module.exports = function(app) {
     res.send('server is active')
   })
 
-
   /**
    * Notes: 
    * - reviews and ratings are handled in accounts module
    * - treasury does not have its own routes, it is used by routes defined in accounts module
    */
-  require('./accounts/accountRoutes')(app);
-  require('./goals/goalRoutes')(app);
-  require('./transactions/transactionRoutes')(app);
-  require('./messaging/messagingRoutes')(app);
-  require('./reports/reportRoutes')(app);
+  require('./main_modules/accounts/accountRoutes')(app);
+  require('./main_modules/goals/goalRoutes')(app);
+  require('./main_modules/transactions/transactionRoutes')(app);
+  require('./main_modules/messaging/messagingRoutes')(app);
+  require('./main_modules/reports/reportRoutes')(app);
 }
