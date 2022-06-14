7bf9d3c97b8d94270d3f28075e0ce8139ab1ff84
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-06-14 15:36:52 -0700

    refactor: separate routes into files by their module

--- server/routes.js
+++ server/routes.js
@@ -1,131 +1,17 @@
-const routes = (app) => {
-  app.route('/')
-    .get((req, res) => {
-      res.send('Hello World!')
-    })
+module.exports = function(app) {
+  app.get('/',(req, res) => {
+    res.send('server is active')
+  })
 
-  app.route('/users')
-    .post((req,res) => {
-      res.send('posting to /users');
-    })
 
-  app.route('/users/:userId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .put((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/goals/:userId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .post((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/goals/:userId/:goalId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .put((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/transactions/:userId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .post((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/transactions/:userId/:transactionId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .put((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/messages/:accountId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/messages') //requires query params 'sender' and 'receiver'
-    .post((req,res) => {
-      let sender = req.query.sender;
-      let receiver = req.query.receiver;
-      res.send(sender + ' & ' + receiver);
-    })
-    .delete((req,res) => {
-      let sender = req.query.sender;
-      let receiver = req.query.receiver;
-      res.send(sender + ' & ' + receiver);
-    })
-
-  app.route('/reports/:userId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .post((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/transactions/:userId/reportId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .put((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/reviews/:accountantId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .post((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/accountants')
-    .post((req,res) => {
-      res.send(req.params);
-    })
-
-  app.route('/accountants/:accountantId')
-    .get((req,res) => {
-      res.send(req.params);
-    })
-    .put((req,res) => {
-      res.send(req.params);
-    })
-    .delete((req,res) => {
-      res.send(req.params);
-    })
+  /**
+   * Notes: 
+   * - reviews and ratings are handled in accounts module
+   * - treasury does not have its own routes, it is used by routes defined in accounts module
+   */
+  require('./accounts/accountRoutes')(app);
+  require('./goals/goalRoutes')(app);
+  require('./transactions/transactionRoutes')(app);
+  require('./messaging/messagingRoutes')(app);
+  require('./reports/reportRoutes')(app);
 }
-
-module.exports = routes;
