b1992fa16e90fb5272039e5af84e0704bb1ac537
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-06-15 20:02:09 -0700

    dev: create accounts and goals models

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000;
-
-require('./routes')(app);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
