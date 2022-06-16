c407d7f79c88d26fa7002358b66b8c9ca996ad42
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-06-15 20:02:09 -0700

    dev: create accounts and goals models

--- server/src/main_modules/accounts/accountRoutes.js
+++ server/src/main_modules/accounts/accountRoutes.js
@@ -1,8 +1,30 @@
+const {User,Accountant} = require('./models');
+
+const user1 = new User({
+  firstname: 'Dean',
+  lastname: 'Yang',
+  email: 'test123@gmail.com',
+  id: '1',
+  subscribed: false,
+  secretKey: 'secret',
+})
+const accountant1 = new Accountant({
+  firstname: 'Dean',
+  lastname: 'Yang',
+  email: 'test123@gmail.com',
+  id: '2',
+  // secretKey: '2',
+  reviews: []
+})
+
 module.exports = function(app) {
   app.route('/users')
     .post((req,res) => {
-      res.send('posting to /users');
-    })
+      user1.save(e => {
+        if(e) {res.send('save unsuccessful, check your fields');}
+        else res.send('new user is saved');
+      })
+    });
 
   app.route('/users/:userId')
     .get((req,res) => {
@@ -17,8 +39,11 @@ module.exports = function(app) {
 
   app.route('/accountants')
     .post((req,res) => {
-      res.send(req.params);
-    })
+      accountant1.save(e => {
+        if(e) {res.send(e);}
+        else res.send('new accountant is saved');
+      })
+    });
 
   app.route('/accountants/:accountantId')
     .get((req,res) => {
