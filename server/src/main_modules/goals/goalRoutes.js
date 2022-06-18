7644c32bb1c1c9c01bb39dff9b537a647e8317f1
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-06-18 12:32:32 -0700

    dev: update models to match m4

--- server/src/main_modules/goals/goalRoutes.js
+++ server/src/main_modules/goals/goalRoutes.js
@@ -1,17 +1,6 @@
-const Frequency = require('../../utils/types/frequency');
 const {UserGoal} = require('./models');
 
-const goal = {
-  id: '1',
-  title: 'Buy a House',
-  mainCategory: 'long_term',
-  dateSet: new Date('December 17, 1995 03:24:00'),
-  deadline: new Date('December 17, 1999 03:24:00'),
-  requiredAmount: 10000,
-  assignedAmount: 500,
-  availableAmount: 500,
-  frequency: 'once'
-}
+
 
 module.exports = function(app) {
   app.route('/goals/:userId')
