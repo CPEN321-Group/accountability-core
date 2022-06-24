b3439f1a680c2b2a710c6a990287a52cc94e924d
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-06-23 17:06:13 -0700

    dev: rename files to js convention and implement get/post/put user endpoints

--- server/src/utils/types/mongoRequired.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports = {
-  r_string: {type: String, required: true},
-  r_bool:{type: Boolean, required: true},
-  r_num: {type: Number, required: true},
-  r_date: {type: Date, require: true}
-}
