738b16b5516bec66de4dd6fe227f411a3b517f67
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-07-02 18:15:32 -0700

    dev: add socketio to dependencies

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
+const http = require('http');
 const express = require('express')
-const app = express()
 const port = 8000;
 const cors = require('cors');
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app);
+const io = socketio(server)
 
 app.use(express.json());
 app.use(cors());
 
+io.on('connection', socket => {
+  console.log('new ws connection')
+})
 
 require('./routes')(app);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
