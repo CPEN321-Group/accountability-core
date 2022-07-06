91e2e00c4072840e729f593579fc53961d333cb3
liuyishengalan <liuyishengalan@hotmail.com>
2022-07-06 06:32:13 -0700

    frontend/UI: dev: goal frontend done

---
+++ android/app/src/main/java/com/cpen321group/accountability/mainScreen/dashboard/functionpack/GoalUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.cpen321group.accountability.mainScreen.dashboard.functionpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_update);
+    }
+}
