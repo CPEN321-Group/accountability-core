d03e92f0e9af40f1f6d354973ee5fced4378e0e1
YishengLiu <liuyishengalan@hotmail.com>
2022-07-07 17:37:00 +0800

    frontend/UI: dev: transaction creation works for now.

--- android/app/src/main/java/com/cpen321group/accountability/mainScreen/dashboard/functionpack/TransactionSetActivity.java
+++ android/app/src/main/java/com/cpen321group/accountability/mainScreen/dashboard/functionpack/TransactionSetActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.cpen321group.accountability.R;
 import com.cpen321group.accountability.VariableStoration;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -28,5 +31,14 @@ public class TransactionSetActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
         transactionsRV.setLayoutManager(linearLayoutManager);
         transactionsRV.setAdapter(transactionAdapter);
+
+        FloatingActionButton createXtendButton = (FloatingActionButton)findViewById(R.id.floating_action_button_transaction);
+        createXtendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goalCreateIntent = new Intent(TransactionSetActivity.this, TransactionCreateActivity.class);
+                startActivity(goalCreateIntent);
+            }
+        });
     }
 }
