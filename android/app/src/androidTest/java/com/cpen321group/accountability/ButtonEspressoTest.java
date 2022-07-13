1dfaeb47087ac189cbc89b43af3a44dd518acf64
YishengLiu <liuyishengalan@hotmail.com>
2022-07-13 22:46:09 +0800

    frontend/UI: dev: update test case

--- android/app/src/androidTest/java/com/cpen321group/accountability/ButtonEspressoTest.java
+++ android/app/src/androidTest/java/com/cpen321group/accountability/ButtonEspressoTest.java
@@ -3,14 +3,16 @@ package com.cpen321group.accountability;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 
+import com.cpen321group.accountability.welcome.RegisterActivity;
 import com.cpen321group.accountability.welcome.WelcomeActivity;
 
 import org.junit.Rule;
@@ -22,12 +24,15 @@ import org.junit.runner.RunWith;
 public class ButtonEspressoTest {
 
     @Rule
-    public ActivityTestRule<WelcomeActivity> activityRule =
-            new ActivityTestRule<>(WelcomeActivity.class);
+    public IntentsTestRule<WelcomeActivity> intentsTestRule =
+            new IntentsTestRule<>(WelcomeActivity.class);
 
     @Test
-    public void testLogin() {
+    public void testRegisterButton() {
         onView(withId(R.id.welcome_register)).perform(click());
+
+        // Activity under test is now finished.
+        intended(hasComponent(RegisterActivity.class.getName()));
         onView(withId(R.id.sidn_in_view)).check(matches(withText("Register")));
     }
 }
