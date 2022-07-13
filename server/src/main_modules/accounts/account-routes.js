80eb6da159982d4b297fa4eaec1893aa2ee6674e
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-07-12 18:17:25 -0700

    Backend jest setup and refactor accounts module, fix null check (#61)
    * refactor: modify account-store interfaces for readability
    
    * fix: fix null/undefined check for fields
    
    * dev: create accounts mock
    
    * dev: add accounts module null checks and documentation
    
    * fix: add isAccountant check for write review
    
    * fix: add isAccountant check for create/update subscription

--- server/src/main_modules/accounts/account-routes.js
+++ server/src/main_modules/accounts/account-routes.js
@@ -1,88 +1,57 @@
 require('dotenv').config();
-const { getDefinedFields } = require.main.require("./utils/get-defined-fields");
-const { fieldsAreNotNull } = require("../../utils/get-defined-fields");
-const { generateToken, authenticate } = require("./account-auth");
-const { createAccount, findAccountById, updateProfile, createReview, deleteAccount, createSubscription, updateSubscription } = require("./account-store");
-const { Account } = require('./models');
-
-const _ = require.main.require('./utils/tests/model-samples')
+const { createAccount, updateProfile, createReview, deleteAccount, createSubscription, updateSubscription, findAccountants, findAccount } = require("./account-store");
 
 module.exports = function(app) {
   app.route('/accounts')
     .post(async (req,res,next) => {
-      const df = getDefinedFields(req.query);
-      const {accountId,avatar,firstname,lastname,email,age,profession,isAccountant} = df;
-      if (!fieldsAreNotNull({accountId,firstname,lastname,email,age,profession,isAccountant})) {
-        return next(new Error('missing params'));
-      }
-      await createAccount({
-        accountId,
-        profile: { avatar,firstname,lastname,email,age,profession},
-        isAccountant
-      }, (err,newAccount) => {
-        if (err) { return next(err); }
-        res.json(newAccount)
-        
+      await createAccount(req.query, (status,returnData) => {
+        res.status(status).json(returnData);
       })
     });
 
-  app.get('/accounts/accountants',async (req,res,next) => { //fetch all accountants
-    try {
-      const foundAccounts = await Account.find({isAccountant: true});
-      res.status(200).json(foundAccounts);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  app.get('/accounts/accountants', async (req,res) => { //fetch all accountants
+    await findAccountants((status,returnData) => {
+      res.status(status).json(returnData);
+    })
   })
 
   app.route('/accounts/:accountId')
-    .get((req,res,next) => {
-      Account.findOne({accountId: req.params.accountId},(err,account) => {
-        if (err) return next(err);
-        if (!account) return res.status(404).end('account not found');
-        return res.json(account);
+    .get(async (req,res) => {
+      await findAccount(req.params.accountId, (status,returnData) => {
+        res.status(status).json(returnData);
       })
     })
-    .put((req,res,next) => {
-      updateProfile(req.params.accountId,req.query, (err,account) => {
-        if (err) return next(err);
-        if (!account) return res.status(404).end('account not found');
-        return res.json(account)
+    .put(async (req,res) => {
+      await updateProfile(req.params.accountId, req.query, (status,returnData) => {
+        res.status(status).json(returnData);
       })
     })
-    .delete(async (req,res,next) => {
-      const deletedAccount = await deleteAccount(req.params.accountId);
-      if (!deletedAccount) return res.status(404).end('account not found')
-      return res.end('account deleted');
+    .delete(async (req,res) => {
+      await deleteAccount(req.params.accountId, (status,returnData) => {
+        res.status(status).json(returnData);
+      })
     })
 
   app.route('/reviews/:accountantId')
-    .post((req,res,next) => {
-      const {accountantId} = req.params;
-      const df = getDefinedFields(req.query);
-      const {authorId,date,rating,title,content} = df;
-      if (!fieldsAreNotNull({authorId,date,rating,title})) { return next(new Error('missing params'))}
-      createReview(accountantId,df, (err,account) => {
-        if (err) return next(err);
-        if (!account) return res.status(404).end('account not found');
-        return res.json(account)
-      })
+    .post(async (req,res) => {
+      await createReview(
+        req.params.accountantId, req.query, 
+        (status,returnData) => {
+          res.status(status).json(returnData);
+        })
     })
   app.route('/subscription/:accountId')
-    .post((req,res,next) => {
-      const {accountId} = req.params;
-      createSubscription(accountId,req.query, (err,account) => {
-        if (err) return next(err);
-        if (!account) return res.status(404).end('account not found');
-        return res.json(account)
-      })
+    .post(async (req,res) => {
+      await createSubscription(
+        req.params.accountId, req.query, 
+        (status,returnData) => {
+          res.status(status).json(returnData);
+        })
     })
-    .put((req,res,next) => {
-      const {accountId} = req.params;
-      updateSubscription(accountId,req.query, (err,account) => {
-        if (err) return next(err);
-        if (!account) return res.status(404).end('account not found');
-        return res.json(account)
-      })
+    .put(async (req,res) => {
+      await updateSubscription(req.params.accountId,req.query, 
+        (status,returnData) => {
+          res.status(status).json(returnData);
+        })
     })
 }
