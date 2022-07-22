021059baf14c7aa750e9af745ff3e25761b9876f
thongn98 <thongn29798@gmail.com>
2022-07-22 07:03:53 +0700

    fix: change deadline validator function

--- server/src/main_modules/goals/models.js
+++ server/src/main_modules/goals/models.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const { r_string, r_num, r_date } = require('../../utils/types/mongo-required');
 const goalDB = mongoose.createConnection((process.env.MONGO_BASE_URL || 'mongodb://localhost') + '/goalDB')
-const { isPastDate } = require('../../utils/checks/date-check');
 
 const goalSchema = new mongoose.Schema({
   title: r_string,
@@ -17,7 +16,10 @@ const goalSchema = new mongoose.Schema({
   deadline: {
     ...r_date,
     validate: {
-      validator: isPastDate
+      validator: function(v) {
+        return v.getTime() > Date.now()
+      },
+      message: "Deadline must be in the future"
     }
   },
 },{timestamps:true});
