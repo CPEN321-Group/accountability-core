c5e1eb00b8b5fc6711fa3ce7a0fb717743dd4a4e
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-07-23 18:21:24 -0700

    Revert mock changes
    This reverts commit 91b5c023ef1e6ed2d8ad8b04760b87ce95fb44ce. mock edits

--- server/src/main_modules/reports/__mocks__/report-models.js
+++ server/src/main_modules/reports/__mocks__/report-models.js
@@ -1,28 +1,14 @@
-const mongoose = require('mongoose');
-const {r_string,r_num,r_date} = require('../../../utils/types/mongo-required');
-const { goalSchema } = require('../../goals/goal-models');
-const { transactionSchema } = require('../../transactions/transaction-models');
-const reportDB = mongoose.createConnection((process.env.MONGO_BASE_URL || 'mongodb://localhost') + '/reportDB')
+class UserReport {
+  constructor(fields){}
+  save(){}
+  static deleteOne(){}
+}
 
-const reportSchema = new mongoose.Schema({
-  monthYear: r_date,
-  income: [transactionSchema],
-  spendings: [transactionSchema],
-  savings: {
-    ...r_num,
-    min: 0
-  },
-  goalsInProgress: [goalSchema],
-  recommendations: String
-})
+class Report {
+  constructor(fields){}
+  save(){}
+  static deleteOne(fields){}
+}
 
-const userReportSchema = new mongoose.Schema({
-  userId: r_string,
-  accountantId: String,
-  reports: {type: [reportSchema],default: []}
-})
 
-const Report = reportDB.model('Report', reportSchema);
-const UserReport = reportDB.model('UserReport', userReportSchema);
-
-module.exports = {UserReport,Report};
+module.exports = {UserReport};
--- server/src/main_modules/transactions/__mocks__/transaction-models.js
+++ server/src/main_modules/transactions/__mocks__/transaction-models.js
@@ -1,37 +1,7 @@
-const mongoose = require('mongoose');
-const {r_string,r_num}= require('../../../utils/types/mongo-required');
-const transactionDB = mongoose.createConnection((process.env.MONGO_BASE_URL || 'mongodb://localhost') + '/transactionDB')
+class UserTransaction {
+  constructor(fields) {}
+  save(){}
+  static deleteOne(fields){}
+}
 
-const transactionSchema = new mongoose.Schema({
-  title: r_string,
-  category: r_string,
-  date: {
-    type: Date, 
-    required: true, 
-    default: new Date(),
-    validate: {
-      validator: function(v) {
-        return v.getTime() <= Date.now()
-      },
-      message: "Transaction date cannot be in the future"
-    }
-  },
-  amount: {
-    ...r_num,
-    min: 0
-  },
-  isIncome: {type: Boolean, required: true,default: false},
-  receipt: String,
-  plaidTransactionId: String
-})
-
-const userTransactionSchema = new mongoose.Schema({
-  userId: r_string,
-  transactions: {type: [transactionSchema], default: []}
-})
-
-const Transaction = transactionDB.model('Transaction',transactionSchema);
-const UserTransaction = transactionDB.model('UserTransaction',userTransactionSchema)
-
-
-module.exports = {UserTransaction, transactionSchema,Transaction};
+module.exports = {UserTransaction}
