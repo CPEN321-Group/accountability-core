24e5fce827d2ced48da1dc561e02d0fcf9cc6c70
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-07-23 20:33:04 -0700

    Update accounts-api.test.js

--- server/src/jest-test/integration-tests/accounts-api.test.js
+++ server/src/jest-test/integration-tests/accounts-api.test.js
@@ -14,6 +14,10 @@ const accountFields = {
   profession: 'Student',
   isAccountant: false
 }
+const newAccountFields = {
+  ...accountFields,
+  accountId: nonExistingId
+}
 
 const accountantFields = {
   accountId: '1456',
@@ -25,7 +29,7 @@ const accountantFields = {
   isAccountant: true
 }
 const reviewFields = {
-  authorId: '1234',
+  authorId: existingId,
   rating: 5,
   date: 'Jun 2022',
   title: 'Good work',
@@ -50,6 +54,7 @@ describe('find an account', () => {
     expect(res.body).toHaveProperty('accountId');
   }) 
   test('account does not exist', async () => {
+    await request(server).delete('/accounts/' + nonExistingId);
     const res = await request(server).get('/accounts/' + nonExistingId);
 
     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
@@ -69,13 +74,65 @@ describe('find accountants', () => {
 })
 
 describe('create an account', () => {
-  test('create an account with', async () => {
-    const res = await request(server).get('/accounts/accountants');
+  test('create an account with an existing accountId', async () => {
+    const res = await request(server).post('/accounts').query({...accountFields});
+
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('name', 'ValidationError');
+  }) 
+  test('create an account with invalid parameters', async () => {
+    const res = await request(server).post('/accounts').query({test: 'test'});
+
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('name', 'ValidationError');
+  }) 
+  test('create an account with new accountId', async () => {
+    await request(server).delete('/accounts/' + nonExistingId);
+    const res = await request(server).post('/accounts').query(newAccountFields);
 
     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
     expect(res.statusCode).toBe(200);
-    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body).toHaveProperty('accountId', newAccountFields.accountId);
+
+    await request(server).delete('/accounts/' + newAccountFields.accountId);
   }) 
+})
+
+describe('update account profile', () => {
+  test('update profile of an existing account', async () => {
+    const res = await request(server).put('/accounts/' + existingId).query({firstname: 'John'});
+
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('profile');
+    expect(res.body.profile).toHaveProperty('firstname', 'John')
+  })
+  test('update profile of a non-existing account', async () => {
+    const res = await request(server).put('/accounts/' + nonExistingId).query({firstname: 'John'});
+
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toHaveProperty('name', 'NotFoundError');
+  })
+  test('update profile with no parameters to update', async () => {
+    const res = await request(server).put('/accounts/' + existingId);
+
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('profile');
+  })
+  test('update profile of an account that doesn\'t belong to the user', async () => {
+    const res = await request(server).put('/accounts/' + existingId).query({
+      firstname: 'John',
+      token: 'invalid-token'
+    });
+    console.log(res.body);
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(403);
+    expect(res.body).toHaveProperty('name', 'ForbiddenRequestError');
+  })
 })
 
 afterAll((done) => {
