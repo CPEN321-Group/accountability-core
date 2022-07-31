06f3cec90fb5f4615a715918a47abbe9313e0323
thongn98 <thongn29798@gmail.com>
2022-07-31 15:27:43 +0700

    devops: upgrade eks cluster

--- tf/eks.tf
+++ tf/eks.tf
@@ -17,7 +17,14 @@ module "eks" {
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 3
+      asg_desired_capacity          = 2
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
     },
   ]
 }
